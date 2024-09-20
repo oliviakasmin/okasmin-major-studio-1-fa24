ad0e9c2f26c599429ccc15c8053e5156fd578d47
Olivia Kasmin <oliviakasmin@gmail.com>
2024-09-20 10:36:25 -0400

    original commit from data_viz boilerplate as starting point for project

---
+++ quantitative/main.js
@@ -0,0 +1,36 @@
+import "./style.css";
+import * as d3 from "d3";
+// const getDimensions = require("./utils");
+
+const getData = async () => {
+	const data = await d3.json("../fetch_download_data/data.json");
+	return data;
+};
+
+const data = await getData();
+
+// render images of first 5 images for prototype test
+const testData = data.slice(0, 5);
+
+const margin = { top: 100, right: 50, bottom: 100, left: 80 };
+const width = 1400;
+const height = 700;
+
+// getDimensions();
+
+const svg = d3
+	.select("body")
+	.append("svg")
+	.attr("width", width)
+	.attr("height", height);
+
+svg
+	.append("g")
+	.selectAll("image")
+	.data(testData)
+	.join("image")
+	.attr("xlink:href", (d) => d.thumbnail)
+	.attr("x", (d, i) => 10 + 100 * i)
+	.attr("y", 10)
+	.attr("width", 100)
+	.attr("height", 100);
